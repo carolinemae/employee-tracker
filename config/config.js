6aa0ee68f604d88b03d62a1e306030766a6676aa
Carol <carolinemaee@live.com>
2022-08-29 11:59:10 +0930

    comments added

--- config/config.js
+++ config/config.js
@@ -11,8 +11,10 @@ config = {
 
 const connection = mysql.createConnection(config);
 
+// Connect to mysql and use credentials above (.env used to save my password)
 connection.connect();
 
+// Export connection to be used in index.js file
 module.exports = {
      connection : mysql.createConnection(config) 
 } 
--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const config = require('./config/config');
 const table = require('console.table');
 const connection = config.connection;
 
+// Main menu
 const mainMenu = [  
     {
         type: 'list',
@@ -23,6 +24,7 @@ const mainMenu = [
     }
 ];
 
+// Asks user if they want to go back to the main menu
 const somethingElse = [
     {
         type: 'list', 
@@ -32,6 +34,7 @@ const somethingElse = [
     }
 ]
 
+// When main menu option is selected go to relevant function
 function prompts(){
     inquirer.prompt(mainMenu).then((answer) => {
         if (answer.mainMenu == 0) {
@@ -60,6 +63,7 @@ function prompts(){
     })
 }
 
+// Function to ask if the user would like to do something else
 function doSomethingElse() {
     inquirer.prompt(somethingElse).then((answer) => {
         if (answer.somethingElse == 'Y') {
@@ -70,6 +74,7 @@ function doSomethingElse() {
     })
 }
 
+// Function to view all employees by querying the database for information
 function viewAllEmployees() {
     connection.query(
         `SELECT e.id, e.first_name, e.last_name, roles.title, departments.dept_name AS department, roles.salary, CONCAT(m.first_name,' ',m.last_name) AS manager
@@ -78,11 +83,12 @@ function viewAllEmployees() {
         LEFT JOIN roles ON e.role_id = roles.id
         LEFT JOIN departments ON departments.id = roles.department_id`, 
         function(err, res) {
-        console.table(res);
-        doSomethingElse();
+            console.table(res); //print the result in table format
+            doSomethingElse(); // then do something else
     })
 }
 
+// Add employee by querying the db for employee list then inquirer prompts
 async function addEmployee() {
     connection.query(`SELECT id AS value, title AS name from roles; SELECT id AS value, CONCAT(first_name,' ',last_name) AS name FROM employees;`, function(err, results) {
         inquirer.prompt([
@@ -90,7 +96,7 @@ async function addEmployee() {
             {type: 'input',     name: 'newEmpLast',     message: 'Last name:'},
             {type: 'list',      name: 'newEmpRole',     message: 'Job role?',               choices: results[0]},
             {type: 'list',      name: 'newEmpManager',  message: 'Who is their manager?',   choices: results[1]}
-        ]).then((answers) => {
+        ]).then((answers) => { //query db to insert into employee table
             console.log("\x1b[36m",'Employee added!',"\x1b[36m");
             connection.query(`INSERT INTO employees (first_name, last_name, role_id, manager_id) VALUES ("${answers.newEmpFirst}","${answers.newEmpLast}","${answers.newEmpRole}","${answers.newEmpManager}")`);
             doSomethingElse();
@@ -98,13 +104,14 @@ async function addEmployee() {
     })
 }
 
+// Query db to view employees before updating
 function updateRole() {
     connection.query(`SELECT id AS value, CONCAT(first_name,' ',last_name) AS name FROM employees; SELECT roles.id AS value, roles.title AS name FROM roles;`, function(err, results) {
         inquirer.prompt([
             {type: 'list', name: 'updateRole',      message: 'Select employee:',    choices: results[0]},
             {type: 'list', name: 'newRole',         message: 'Updated role:',       choices: results[1]},
             {type: 'list', name: 'updateManager',   message: 'Update manager?',     choices: [{value: 'Y', name: 'Yes'}, {value: 'N', name: 'No'}]}
-        ]).then((answers) => {
+        ]).then((answers) => { // once user has selected an employee and answers prompts, query database some more
             let updatingEmployee = answers.updateRole;
             connection.query(`UPDATE employees SET role_id = "${answers.newRole}" WHERE id = ${answers.updateRole}`);
             if (answers.updateManager == 'Y') {
@@ -125,6 +132,7 @@ function updateRole() {
     })
 }
 
+// Query the db to view all roles, display as table and then ask user to do something else
 function viewRoles() {
     connection.query('SELECT roles.id, roles.title, departments.dept_name AS department, roles.salary FROM roles JOIN departments ON roles.department_id = departments.id ORDER BY roles.id', function(err, res) {
         console.table(res);
@@ -132,13 +140,14 @@ function viewRoles() {
     })
 }
 
+// Function to add role, query list of departments to add the new role to a department
 function addRole() {
     connection.query(`SELECT id AS value, dept_name AS name FROM departments`, function(err, res) {
         inquirer.prompt([
             {type: 'input', name: 'newRole', message: 'New role name:'},
             {type: 'input', name: 'newSalary', message: 'Salary:'},
             {type: 'list', name: 'addToDept', message: 'Add to department:', choices: res}
-        ]).then((answers) => {
+        ]).then((answers) => { // insert into the roles table and then do something else
             console.log("\x1b[36m",'Role added!',"\x1b[36m");
             connection.query(`INSERT INTO roles (title, salary, department_id) VALUES ("${answers.newRole}",${answers.newSalary},${answers.addToDept})`);
             doSomethingElse();
@@ -146,6 +155,7 @@ function addRole() {
     })
 }
 
+// Function to view departments table by querying db
 function viewDepartments() {
     connection.query('SELECT id, dept_name AS department FROM departments', function(err, res) {
         console.table(res);
@@ -153,10 +163,11 @@ function viewDepartments() {
     })
 }
 
+// Add department by using inquirer prompts
 function addDepartment() {
     inquirer.prompt([
         {type: 'input', name: 'newDepartment', message: 'Name of department?'}
-    ]).then((answer) => {
+    ]).then((answer) => { // then query db to insert into table
         const sql = `INSERT INTO departments (dept_name) VALUES (?);`
         const params = [`${answer.newDepartment}`];
 
@@ -167,11 +178,12 @@ function addDepartment() {
     })
 }
 
+// Query db for list of employees to select one to delete
 function deleteEmployee(){
     connection.query(`SELECT id AS value, CONCAT(first_name,' ',last_name) AS name FROM employees`, function(err, res) {
         inquirer.prompt([
             {type: 'list', name: 'deleteEmp', message: 'Select employee to remove:', choices: res}
-        ]).then((answers) => {
+        ]).then((answers) => { // then delete the selected employee by id
             console.log("\x1b[36m",`Employee deleted!`,"\x1b[36m");
             connection.query(`DELETE FROM employees WHERE id = ${answers.deleteEmp}`);
             doSomethingElse();
@@ -179,11 +191,12 @@ function deleteEmployee(){
     })
 }
 
+// Query db for list of roles to select one to delete
 function deleteRole(){
     connection.query(`SELECT id AS value, title AS name FROM roles`, function(err, res) {
         inquirer.prompt([
             {type: 'list', name: 'deleteRole', message: 'Select role to remove:', choices: res}
-        ]).then((answers) => {
+        ]).then((answers) => { // then delete the selected role by id
             console.log("\x1b[36m",`Role deleted!`,"\x1b[36m");
             connection.query(`DELETE FROM roles WHERE id = ${answers.deleteRole}`);
             doSomethingElse();
@@ -191,11 +204,12 @@ function deleteRole(){
     })
 }
 
+// Query db for list of departments to select one to delete
 function deleteDepartment(){
     connection.query(`SELECT id AS value, dept_name AS name FROM departments`, function(err, res) {
         inquirer.prompt([
             {type: 'list', name: 'deleteDept', message: 'Select department to remove:', choices: res}
-        ]).then((answers) => {
+        ]).then((answers) => { // then delete the selected department by id
             console.log("\x1b[36m",`Department deleted!`,"\x1b[36m");
             connection.query(`DELETE FROM departments WHERE id = ${answers.deleteDept}`);
             doSomethingElse();
@@ -203,4 +217,5 @@ function deleteDepartment(){
     })
 }
 
+// Run the prompts function at start of application
 prompts();
